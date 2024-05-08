8dba3fa7c903120f42c817aed1f3e6589925a494
berth21gg <19031032@itcelaya.edu.mx>
2024-05-08 17:07:30 -0600

    Creación de CRUD

---
+++ lib/database/crud.dart
@@ -0,0 +1,29 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:taller_sqlite_flutter/database/db.dart';
+
+abstract class CRUD {
+  String table;
+
+  CRUD(this.table);
+
+  Future<Database> get database async {
+    return await DB().open();
+  }
+
+  // Sentencias preparadas
+  query(String sql, {List<dynamic> arguments = const []}) async {
+    return (await database).rawQuery(sql, arguments);
+  }
+
+  update(Map<String, dynamic> data) async {
+    return (await database).update(table, data);
+  }
+
+  create(Map<String, dynamic> data) async {
+    return (await database).insert(table, data);
+  }
+
+  delete(int id) async {
+    return (await database).delete(table, where: 'id = ?', whereArgs: [id]);
+  }
+}
